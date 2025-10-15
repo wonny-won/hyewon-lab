b4d5b17a5a986e2fcd66e5ba2d4eee52bb9f3398
wonny <97152701+wonny-won@users.noreply.github.com>
2025-10-15 13:21:16 +0900

    feat: typo & intro 시멘틱 태그 적용

--- apps/hyewon/src/views/layout/intro/intro.tsx
+++ apps/hyewon/src/views/layout/intro/intro.tsx
@@ -8,12 +8,16 @@ const Intro = () => {
 	return (
 		<div className="w-[460px] h-screen fixed py-26 px-30 flex flex-col gap-6 bg-[url('/side-panel-bg.png')]">
 			<Image src={aboutMyProfile.profile} alt='profile img' width={200} height={250} />
-			<Typography variants='display-l'>{aboutMyProfile.name}</Typography>
-			<Typography variants='heading-m'>{aboutMyProfile.position}</Typography>
+			<Typography as='h1' variants='display-l'>
+				{aboutMyProfile.name}
+			</Typography>
+			<Typography as='h2' variants='heading-m'>
+				{aboutMyProfile.position}
+			</Typography>
 			<ul>
 				<li>
 					{aboutMyProfile.summaryTxt.map((i, idx) => (
-						<Typography variants='body-m' key={idx} className='flex gap-1 items-center py-2'>
+						<Typography as='p' variants='body-m' key={idx} className='flex gap-1 items-center py-2'>
 							<Icons iconName='DancheongFlower' size='20px' />
 							{i}
 						</Typography>
