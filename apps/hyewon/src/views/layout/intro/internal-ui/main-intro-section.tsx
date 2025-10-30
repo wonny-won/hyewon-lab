7511dca9ace830730427de7229dca585bb6ac933
wonny <97152701+wonny-won@users.noreply.github.com>
2025-10-30 23:13:35 +0900

    feat: liquid section card 컴포넌트 통일

---
+++ apps/hyewon/src/views/layout/intro/internal-ui/main-intro-section.tsx
@@ -0,0 +1,36 @@
+/** @format */
+
+import Image from 'next/image';
+import PrograssUI from './prograss/prograss-ui';
+import { Typography } from '@hyewon/design-system';
+import { aboutMyProfile } from '@/commons/apis/intro';
+
+interface MainInroSectionProps {
+	type?: 'imgCard' | 'txtCard' | 'prograssCard';
+	currNavIdx?: string;
+}
+const MainInroSection = ({ currNavIdx = '' }: MainInroSectionProps) => {
+	return (
+		<>
+			<div className='rounded-[30px] flex gap-5 items-center'>
+				<Image
+					src={aboutMyProfile.profile}
+					alt={'profile img'}
+					width={80}
+					height={80}
+					style={{ borderRadius: '10px' }}
+				/>
+				<div>
+					<Typography as='h1' variants='display-l' color='text-white'>
+						{aboutMyProfile.name}
+					</Typography>
+					<Typography as='h2' variants='body-l-strong' color='text-white'>
+						{aboutMyProfile.position}
+					</Typography>
+				</div>
+			</div>
+			<PrograssUI currNavIdx={currNavIdx} />
+		</>
+	);
+};
+export default MainInroSection;
--- apps/hyewon/src/views/layout/intro/intro.tsx
+++ apps/hyewon/src/views/layout/intro/intro.tsx
@@ -1,10 +1,10 @@
 /** @format */
 
-import { Tags, Typography } from '@hyewon/design-system';
+import { SectionCard, Tags } from '@hyewon/design-system';
 import ListUI from '@/components/list-ui/list-ui';
 import { aboutMyProfile } from '@/commons/apis/intro';
-import LiquidSection from '@/components/liquid-section/liquid-section';
 import { useScrollContext } from '@/commons/context/scroll-context';
+import MainInroSection from './internal-ui/main-intro-section';
 
 const Intro = () => {
 	const { currNavId } = useScrollContext();
@@ -12,24 +12,11 @@ const Intro = () => {
 	return (
 		<header className='w-[60%] h-screen py-25'>
 			<div className='relative flex flex-col gap-6'>
-				<LiquidSection
-					calssName='shrink-0 p-8'
-					type='prograssCard'
-					imgSrc={aboutMyProfile.profile}
-					imgAlt='profile img'
-					currNavIdx={currNavId}>
-					<div>
-						<Typography as='h1' variants='display-l' color='text-white'>
-							{aboutMyProfile.name}
-						</Typography>
-						<Typography as='h2' variants='body-l-strong' color='text-white'>
-							{aboutMyProfile.position}
-						</Typography>
-					</div>
-				</LiquidSection>
+				<SectionCard type='default' styleType='liquid'>
+					<MainInroSection currNavIdx={currNavId} />
+				</SectionCard>
 				<div className='relative flex gap-4'>
-					<LiquidSection calssName='flex flex-wrap h-full max-w-[250px]'>
-						{/* <ListUI direction='virtical' listMap={aboutMyProfile.skillTag} color='text-core-neutral-300' /> */}
+					<SectionCard type='default' styleType='liquid' title='Skills' sectionCardClassName='max-w-[500px]'>
 						<ul className='flex flex-wrap gap-3 justify-center'>
 							{aboutMyProfile.skillTag.map((i, idx) => (
 								<li key={idx}>
@@ -37,15 +24,13 @@ const Intro = () => {
 								</li>
 							))}
 						</ul>
-					</LiquidSection>
-					<div className='flex flex-col gap-5'>
-						<LiquidSection calssName='h-fit max-w-[150px]'>
-							<ListUI direction='horizontal' listMap={aboutMyProfile.contact} color='text-white' />
-						</LiquidSection>
-						<LiquidSection calssName='h-fit max-w-[150px]'>
-							<div>지쳤나요</div>
-						</LiquidSection>
-					</div>
+					</SectionCard>
+					<SectionCard
+						type='default'
+						styleType='liquid'
+						sectionCardClassName='flex items-center flex-wrap h-fit max-w-[150px]'>
+						<ListUI direction='horizontal' listMap={aboutMyProfile.contact} />
+					</SectionCard>
 				</div>
 			</div>
 		</header>
