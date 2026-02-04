5cbf29cfe427515cdea2964a9c0a539692ac1509
wonny <97152701+wonny-won@users.noreply.github.com>
2026-02-04 16:30:59 +0900

    fix: summary data fix

--- apps/hyewon/src/commons/apis/sections/record.ts
+++ apps/hyewon/src/commons/apis/sections/record.ts
@@ -20,19 +20,31 @@ export const recordData = [
 				children: [
 					{
 						id: 'honored-summary-section-1',
-						title: '아너드 자체 웹 서비스 런칭의 전 과정을 맡아 프론트엔드 E2E 단독 런칭을 수행하며 제품 이해와 기술적 시야를 확장했습니다.',
+						title: '아너드 자체 웹 및 신한 제휴 서비스 프론트 파트 E2E 단독 설계 및 개발 / 운영',
 						isImportant: false,
 						children: null,
 					},
 					{
 						id: 'honored-summary-section-2',
-						title: '특히 모노레포 구조를 직접 구축해 디자인 시스템의 재사용성을 획기적으로 높였고, 이 과정에서 발생한 다양한 배포 환경·빌드 오류 트러블슈팅을 해결하며 안정적인 배포 파이프라인을 완성했습니다. ',
+						title: '자체웹 성능 최적화 및 검색엔진 최적화',
 						isImportant: false,
 						children: null,
 					},
 					{
 						id: 'honored-summary-section-3',
-						title: '또한 개발 프로세스를 리드하며 일정·문서화 체계를 도입해 주도적으로 개발 문화를 정립한 경험이 프로젝트의 중심 성과입니다.',
+						title: '신규 어드민 런칭 및 구 어드민 유지보수',
+						isImportant: false,
+						children: null,
+					},
+					{
+						id: 'honored-summary-section-3',
+						title: '웹 프론트엔드 파트 리드로 개발 타임라인 관리 및 배포 주기 관리',
+						isImportant: false,
+						children: null,
+					},
+					{
+						id: 'honored-summary-section-3',
+						title: '문서화 도입 및 개발 시스템 구축으로 개발 환경 개선 기여',
 						isImportant: false,
 						children: null,
 					},
@@ -166,19 +178,25 @@ export const recordData = [
 				children: [
 					{
 						id: 'mayI-summary-section',
-						title: '짧은 기간이었지만 신규 기능 런칭을 중심으로 프론트엔드 설계와 성능 최적화를 경험하며 빠르게 성장했습니다.',
+						title: 'Mash 대시보드 신규 기능 및 코호트 분석 페이지 개발 및 핫픽스 배포',
 						isImportant: false,
 						children: null,
 					},
 					{
 						id: 'mayI-summary-section',
-						title: '단일책임 원칙 적용, 렌더 성능 개선, 네트워크 요청 최적화 등을 통해 서비스 안정성과 확장성을 함께 고려한 개발을 수행했습니다.',
+						title: '확장성을 고려한 프로바이더 설계',
 						isImportant: false,
 						children: null,
 					},
 					{
 						id: 'mayI-summary-section',
-						title: '빠른 성장을 바탕으로 외부 스카웃 제안을 받아 커리어를 확장했습니다.',
+						title: '위젯 및 대시보드 성능 최적화',
+						isImportant: false,
+						children: null,
+					},
+					{
+						id: 'mayI-summary-section',
+						title: '스카웃 제의로 이직',
 						isImportant: false,
 						children: null,
 					},
@@ -321,25 +339,25 @@ export const recordData = [
 				children: [
 					{
 						id: 'teamstone-summary-section',
-						title: '해당 프로젝트를 통해 성능 측정을 하는 방법을 익히고, 어떤 지점이 퍼포먼스 저하를 초래하는지 판단할 수 있는 역량을 갖추게 되었습니다. ',
+						title: 'ontune 시스템 모니터링 대시보드 v5 버전 개발 및 런칭',
 						isImportant: false,
 						children: null,
 					},
 					{
 						id: 'teamstone-summary-section',
-						title: '성능 측정 결과를 토대로 위젯과 주요 페이지의 퍼포먼스를 개선했습니다. ',
+						title: '위젯 및 페이지 성능 측정 결과를 토대로 위젯과 주요 페이지의 퍼포먼스를 개선. ',
 						isImportant: false,
 						children: null,
 					},
 					{
 						id: 'teamstone-summary-section',
-						title: '대표적으로 메모리 누수 제거, 불필요한 전체 리렌더 최소화 등 체감 성능을 높인 개선 사례를 만들었습니다.',
+						title: '테스트를 기반으로 사용자 친화적인 UI/UX 개선 의견 제시 및 반영',
 						isImportant: false,
 						children: null,
 					},
 					{
 						id: 'teamstone-summary-section',
-						title: '이 성과를 기반으로 사내 최단기 주임 승진이라는 결과로 이어졌습니다.',
+						title: '이 성과를 기반으로 사내 최단기 주임 승진',
 						isImportant: false,
 						children: null,
 					},
@@ -671,13 +689,19 @@ export const recordData = [
 				children: [
 					{
 						id: 'dingco-summary-section',
-						title: '인프런과 협력한 부트캠프에서 멘토로 활동하며 수강생에게 [노션 장인] 이라는 영광의 타이틀을 받은 개발자 첫 커리어입니다.',
+						title: '코드캠프 온/오프라인 부트캠프  학습자료 제작  파트 리드 ',
 						isImportant: false,
 						children: null,
 					},
 					{
 						id: 'dingco-summary-section',
-						title: '커리큘럼 개편 회의에 참여해 직접 의견을 내고 개발동향을 추적하며 개발자로서 많은 성장을 일궈낸 핵심 경험이었습니다.',
+						title: '[ 인프런 X 코드캠프 ]  & 코드캠프 온/오프라인 부트캠프 프론트엔드 멘토',
+						isImportant: false,
+						children: null,
+					},
+					{
+						id: 'dingco-summary-section',
+						title: '회사 홈페이지 랜딩페이지 및 부트캠프 사이트 개발 ',
 						isImportant: false,
 						children: null,
 					},
