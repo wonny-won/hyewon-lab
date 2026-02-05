064449e2b837c322a1d650bc84f84ad7e7f4cfe8
wonny <97152701+wonny-won@users.noreply.github.com>
2026-02-05 11:53:18 +0900

    fix: navigation view 수정

--- apps/hyewon/src/views/home/sections/record/internal-ui/image.tsx
+++ apps/hyewon/src/views/home/sections/record/internal-ui/image.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image';
 
 const ImgComp = (data: any) => {
-	console.log(data);
 	return (
 		<Image
 			src={data.data.url || ''}
--- apps/hyewon/src/views/layout/index.tsx
+++ apps/hyewon/src/views/layout/index.tsx
@@ -3,12 +3,16 @@ import Head from 'next/head';
 import React, { ReactNode } from 'react';
 import Footer from './footer/footer';
 import Navigation from './nav/navigation';
+import { useRouter } from 'next/router';
 
 interface LayoutProps {
 	children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+	const excludesNavigationPage = ['/projects/company'];
+	const route = useRouter();
+
 	return (
 		<>
 			<Head>
@@ -19,7 +23,7 @@ const Layout = ({ children }: LayoutProps) => {
 				<meta name='description' content='프론트엔드 개발자 정혜원의 포트폴리오 사이트입니다.' />
 			</Head>
 			<div className='w-full overflow-hidden flex flex-col'>
-				<Navigation />
+				{!excludesNavigationPage.some((i) => route.asPath.includes(i)) && <Navigation />}
 				<main className='w-full h-full overflow-hidden px-50 max-[1100px]:px-20 max-[1200px]:px-25'>
 					{children}
 				</main>
