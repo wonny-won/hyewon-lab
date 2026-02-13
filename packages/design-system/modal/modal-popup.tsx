96c12e4cdb8e883f21440d49657c75c8f937ea35
wonny <97152701+wonny-won@users.noreply.github.com>
2026-02-13 11:16:00 +0900

    fix: 모달 close 버튼 위치 수정

--- packages/design-system/modal/modal-popup.tsx
+++ packages/design-system/modal/modal-popup.tsx
@@ -12,13 +12,15 @@ const ModalPopup = ({ setIsOpen, children }: ModalPopupProps) => {
 	return (
 		<div className='fixed inset-0 flex justify-center items-center z-30'>
 			<div className='w-[70%] h-fit p-10 bg-slate-950 text-white rounded-lg'>
-				<button
-					className='hover: cursor-pointer'
-					onClick={() => {
-						setIsOpen((prev) => !prev);
-					}}>
-					<Icons iconName='Close' />
-				</button>
+				<div className='w-full flex mt-2 mb-4'>
+					<button
+						className='hover: cursor-pointer ml-auto'
+						onClick={() => {
+							setIsOpen((prev) => !prev);
+						}}>
+						<Icons iconName='Close' />
+					</button>
+				</div>
 				<div>{children}</div>
 			</div>
 		</div>
