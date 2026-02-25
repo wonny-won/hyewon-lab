39e872e3889c3b1be17ed6c8bbad149fc8f6cf8d
wonny <97152701+wonny-won@users.noreply.github.com>
2026-02-25 20:44:15 +0900

    fix: 준비중 뷰 수정

--- apps/hyewon/src/components/warnning-info/ui/warnning-info.tsx
+++ apps/hyewon/src/components/warnning-info/ui/warnning-info.tsx
@@ -4,7 +4,7 @@ import { Icons, Typography } from '@hyewon/design-system';
 
 const WarnningInfo = ({ infoTxt }: { infoTxt: { main: string; sub: string | null } }) => {
 	return (
-		<div className='flex flex-col items-center justify-center w-full h-fit p-30 gap-4'>
+		<div className='flex flex-col items-center justify-center w-full h-fit p-30 max-sm:pt-30 max-sm:px-0 gap-4'>
 			<Icons iconName='InfoIcon' strokeWidth='1.2' size='35' color='#8e9799' />
 			<div className='flex flex-col gap-1 items-center'>
 				<Typography variants='label-l' color='text-neutral-400'>
